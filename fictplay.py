8a8269b1b8ffb48a572661a86e3a842bd95a21e4
6379235442 <6379235442@mail.ecc.u-tokyo.ac.jp>
2014-06-17 16:01:43 +0900

    期待利得を行列計算で求めるようにしました。

--- fictplay.py
+++ fictplay.py
@@ -11,8 +11,8 @@ def fictplay(t):
     gain_1 = np.array([[-1, 1], [1, -1]])   #player1
 	
     for i in range(t):
-        pro_0 = np.array([1-x_0_t[i], x_0_t[i]])      #probability 0 or 1
-        exp_gain_0 = np.dot(gain_0, pro_0)            #expected gain
+        pro_1 = np.array([1-x_0_t[i], x_0_t[i]])      #probability: player1 choice 0 or 1
+        exp_gain_0 = np.dot(gain_0, pro_1)            #expected gain = gain ×　probability
         if exp_gain_0[0] > exp_gain_0[1]:
             a_0_i = 0
         elif exp_gain_0[0] == exp_gain_0[1]:
@@ -20,8 +20,8 @@ def fictplay(t):
         else:
             a_0_i = 1
 
-        pro_1 = np.array([1-x_1_t[i], x_1_t[i]])   
-        exp_gain_1 = np.dot(gain_1, pro_1)
+        pro_0 = np.array([1-x_1_t[i], x_1_t[i]])   
+        exp_gain_1 = np.dot(gain_1, pro_0)
         if exp_gain_1[0] > exp_gain_1[1]:
             a_1_i = 0
         elif exp_gain_1[0] == exp_gain_1[1]:
@@ -37,18 +37,18 @@ def fictplay(t):
     return x_0_t, x_1_t
 
 
-#x_0, x_1 = fictplay(1000)
+x_0_t, x_1_t = fictplay(1000)
          
-#plt.plot(x_0, 'r-', label="x_0(t)")
-#plt.plot(x_1, 'b-', label="x_1(t)")
-#plt.legend()
+plt.plot(x_0_t, 'r-', label="x_0(t)")
+plt.plot(x_1_t, 'b-', label="x_1(t)")
+plt.legend()
 #plt.savefig('fictplay0.png')
-#plt.show()
-
-x_0_t = []
-for i in range(100):
-    x_0, x_1 = fictplay(100)
-    x_0_t.append(x_0[-1])
-plt.hist(x_0_t)
-plt.savefig('fictplay_hist.png')
 plt.show()
+
+#x_0_t = []
+#for i in range(100):
+#    x_0, x_1 = fictplay(100)
+#    x_0_t.append(x_0[-1])
+#plt.hist(x_0_t)
+#plt.savefig('fictplay_hist.png')
+#plt.show()
